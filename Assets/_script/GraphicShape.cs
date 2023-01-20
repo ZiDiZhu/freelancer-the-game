9ed98ab15e53b18a0c0ece38e51d279a335fc5f5
Zi Di <grosswamp@gmail.com>
2023-01-19 22:05:14 -0500

    basic change color mechanic + imported dotween

--- Assets/_script/GraphicShape.cs
+++ Assets/_script/GraphicShape.cs
@@ -10,6 +10,7 @@ public class GraphicShape : MonoBehaviour
 {
     public GameObject[] shape;//shapes in a template, which are buttons
     public GameObject selectedShape;
+    public GameObject[] colorSwatch;
 
     private void Start()
     {
@@ -17,6 +18,13 @@ public class GraphicShape : MonoBehaviour
         {
             int n = i;//to prevent variable capturing
             shape[n].GetComponent<Button>().onClick.AddListener(() => buttonCallBack(shape[n]));//so that clicking on button triggers a callback
+        }
+
+        for(int i = 0; i < colorSwatch.Length; i++)
+        {
+            int n = i;
+            colorSwatch[n].GetComponent<Button>().onClick.AddListener(() => colorSwatchClicked(colorSwatch[n]));
+
         }
 
     }
@@ -28,13 +36,26 @@ public class GraphicShape : MonoBehaviour
         if (selectedShape == myShape)
         {
             selectedShape = null;
-            EventSystem.current.SetSelectedGameObject(null);
+            DeselectShape();
         }
             
         else
             selectedShape = myShape;
     }
 
+    private void colorSwatchClicked(GameObject myColorSwatch)
+    {
+        if (selectedShape != null)
+        {
+            selectedShape.GetComponent<Image>().color = myColorSwatch.GetComponent<Image>().color;
+        }
+    }
+
+    void DeselectShape()
+    {
+        selectedShape = null;
+        EventSystem.current.SetSelectedGameObject(null);
+    }
 
 
 }
