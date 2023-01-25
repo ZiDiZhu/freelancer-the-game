6a40961367b82794e779281620b2173119fe5e08
Nebularswamp <grosswamp@gmail.com>
2023-01-25 00:30:40 -0500

    imported time and weather system

---
+++ Assets/_script/InteractionUtility.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionUtility : MonoBehaviour
+{
+
+    public void ToggleTargetObjectIsActive(GameObject targetObject)
+    {
+        targetObject.SetActive(!targetObject.active);
+    }
+
+}
